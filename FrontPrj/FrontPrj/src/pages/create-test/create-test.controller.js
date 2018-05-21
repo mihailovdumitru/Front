8ff51dfe4a4e75521a9e138dfb9104b13e81af26
mihailovdumitru <mihailovdumitru@gmail.com>
2018-05-22 01:08:51 +0300

    Add answers automatically

--- FrontPrj/FrontPrj/src/pages/create-test/create-test.controller.js
+++ FrontPrj/FrontPrj/src/pages/create-test/create-test.controller.js
@@ -5,7 +5,24 @@ const CONTROLLER_NAME = 'CreateTestController';
 angular
     .module('app')
     .controller(CONTROLLER_NAME, ['$scope', '$location', 'createTestService', ($scope, $location, createTestService) => {
+        var ctrl = $scope;
 
+        init();
+
+        function init() {
+            ctrl.model = [];
+            ctrl.model.question = "";
+            ctrl.model.answers = [];
+        }
+
+        ctrl.save = function () {
+            console.log(ctrl.model.question);
+        }
+
+        ctrl.addAnswer = function () {
+            ctrl.model.answers.push({ content: "" });
+            console.log(ctrl.model.answers);
+        }
 
         $scope.showPopup = function () {
             angular.element(document.querySelector('.popup'))[0].style.display = "block";
--- FrontPrj/FrontPrj/src/pages/create-test/create-test.js
+++ FrontPrj/FrontPrj/src/pages/create-test/create-test.js
@@ -1,23 +1,9 @@
-﻿//import angular from 'angular'
-
-//angular
-//    .module('createTest', [require('angular-route')])
-
+﻿
+import './create-test.css';
 const CONTROLLER_NAME = require('@pages/create-test/create-test.controller.js');
 
-//angular
-//    .module('createTest')
-//    .config(function ($locationProvider, $routeProvider) {
-//        $locationProvider.html5Mode(true);
-//        $routeProvider
-//            .when('/', {
-//                template: require('@pages/create-test/create-test.template.html'),
-//                controller: CONTROLLER_NAME
-//            });
-//    });
 
 (function (angular) {
- //   'use strict';
     angular
         .module('app')
         .config(function ($locationProvider, $routeProvider) {
