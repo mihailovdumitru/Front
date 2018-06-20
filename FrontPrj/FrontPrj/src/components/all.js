13e89560390ed248e8f8f00f8e544b9980980305
DumitruMihailov <Dumitru.Mihailov@ness.com>
2018-06-20 11:16:58 +0300

    remove comments

--- FrontPrj/FrontPrj/src/components/all.js
+++ FrontPrj/FrontPrj/src/components/all.js
@@ -1,5 +1,4 @@
 require('@components/header/header.js');
-// require('@components/alertbox/alertbox.js');
 require('@components/primary-button/primary-button.js');
 require('@components/dropdown/dropdown.js');
 require('@components/popup/popup.js');
@@ -8,4 +7,3 @@ require('@components/checkbox/checkbox.js');
 require('@components/textarea/textarea.js');
 require('@components/radio-button/radio-button.js');
 require('@components/dropdown-multiple-select/dropdown-multiple-select.js');
-// require('@pages/vacancies/vacancies-employee/vacancy-card-em/vacancy-card-em.js');
